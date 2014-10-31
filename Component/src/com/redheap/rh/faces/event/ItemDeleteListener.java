c162d6db33f630e60dfa306908a632dd84a0e756
Wilfred van der Deijl <wilfred@vanderdeijl.com>
2014-10-31 15:59:47 +0100

    Listeners should be interfaces not classes

--- Component/src/com/redheap/rh/faces/event/ItemDeleteListener.java
+++ Component/src/com/redheap/rh/faces/event/ItemDeleteListener.java
@@ -3,16 +3,15 @@ package com.redheap.rh.faces.event;
 import javax.faces.event.AbortProcessingException;
 import javax.faces.event.FacesListener;
 
-public class ItemDeleteListener implements FacesListener {
-    
+public interface ItemDeleteListener extends FacesListener {
+
     //FIXME: PlaceHolder code from the Oracle Example.
+
     /**
      * <p>Process the {@link TagSelectEvent}.</p>
      * @param event fired on click of a tag link
      * @throws AbortProcessingException error processing {@link TagSelectEvent}
      */
-    public void processItemDelete(ItemDeleteEvent event) throws AbortProcessingException{
-        //FIXME: implement.
-        return;
-    }
+    public void processItemDelete(ItemDeleteEvent event) throws AbortProcessingException;
+
 }
